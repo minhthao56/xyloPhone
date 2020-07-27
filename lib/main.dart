19a4c8f53c9d68f6b34ba01652e1e35a0ca6ce08
minhthao <minhthao5648@gmail.com>
2020-07-27 21:26:58 +0700

    Ok nha

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: PagePhone(),
       ),
     ),
@@ -16,19 +17,35 @@ class PagePhone extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void hanldeNote(int number) {
+      final player = AudioCache();
+      player.play('note$number.wav');
+    }
+
+    Expanded bulidKey({Color color, int num}) {
+      return Expanded(
+        child: FlatButton(
+          color: color,
+          onPressed: () {
+            hanldeNote(num);
+          },
+        ),
+      );
+    }
+
     return SafeArea(
-      child: Container(
-          color: Colors.white,
-          child: Center(
-            child: FlatButton(
-              child: Text("Click me!"),
-              onPressed: () {
-                print('ok');
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-            ),
-          )),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          bulidKey(color: Colors.red, num: 1),
+          bulidKey(color: Colors.orange, num: 2),
+          bulidKey(color: Colors.yellow, num: 3),
+          bulidKey(color: Colors.green, num: 4),
+          bulidKey(color: Colors.blue, num: 5),
+          bulidKey(color: Colors.indigo, num: 6),
+          bulidKey(color: Colors.purple, num: 7)
+        ],
+      ),
     );
   }
 }
